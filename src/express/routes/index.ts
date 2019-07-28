9f7179207b78e19e202aa5bdc755fe097295f24a
Pavel Balashov <pavel.balashov1@gmail.com>
2019-07-28 20:08:16 +0200

    Delete unneeded stuff and install tslib

--- src/express/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import rootRouter from './root';
-import error404Router from './404';
-import htmlsRouter from './htmls';
-
-export default {
-  rootRouter,
-  error404Router,
-  htmlsRouter,
-};
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-// import { experiment as fun, } from './experiments';
-import { runServer as fun, } from './express/server';
-
-if (typeof require !== 'undefined' && require.main === module) {
-  fun();
-}
